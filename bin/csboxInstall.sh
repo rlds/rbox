87540cf60670ab200997ca5ad17bf57a419e0db1
吴道睿 <daorui.wu@tongdun.cn>
2018-11-15 14:13:21 +0800

    添加说明

--- bin/csboxInstall.sh
+++ bin/csboxInstall.sh
@@ -8,9 +8,12 @@ if [ -z "$installPath" ];then
    echo "请输入将要安装csbox的文件夹路径"
 else
    echo "csbox将安装的文件夹路径为："${installPath}
+   echo "开始清理已安装部分"
+   rm -rf ${installPath}/bin/csbox
+   rm -rf ${installPath}/static
+   echo "清理完成"
    mkdir -p {${installPath}/log,${installPath}/bin}
    cp csbox ${installPath}/bin/
    cp -rf ../csbox/static ${installPath}
    echo "安装完成可以开始使用了 csbox路径为："${installPath}/bin/csbox
 fi
-
